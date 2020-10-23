e496148ee0c0b3ec2f94674781506ec044a79cb5
tmark's company <779297395@qq.com>
2020-10-23 11:01:58 +0800

    增加希尔排序和基数排序

--- repository/sort_collection.cpp
+++ repository/sort_collection.cpp
@@ -4,14 +4,17 @@
  @ File Name	: sort_collection.cpp
  @ Description	: 排序算法汇总
  ************************************************************************/
-#include <stdlib.h>
-#include <time.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cmath>
 
 #include <iostream>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::cerr;
 
 int Compare(int a, int b) 
 {
@@ -28,7 +31,6 @@ void PrintArray(int * arr, int size)
 
 class Solution {
 public:
-
 	// 冒泡排序 
 	void BubbleSort(int * arr, int size, int (*cmp)(int, int));
 	// 冒泡排序（优化）
@@ -37,6 +39,15 @@ public:
 	void SelectionSort(int * arr, int size, int (*cmp)(int, int));
 	// 插入排序
 	void InsertSort(int * arr, int size, int (*cmp)(int, int));
+	// 希尔排序	
+	void ShellSort(int *arr, int size, int (*cmp)(int, int ));
+	// 基数排序 (计数基数排序)
+	void RadixSort(int *arr, int size, int (*cmp)(int, int ));
+
+private:
+	void ShellInsert(int * arr, int size, int (*cmp)(int, int), int d);
+	void CounterRadixSort(int *from, int * to, int size, int (*cmp)(int, int), int *count, int *pos, int r, int m);
+
 };
 
 void Solution::BubbleSort(int *arr, int size, int (*cmp)(int, int)) 
@@ -111,6 +122,96 @@ void Solution::InsertSort(int *arr, int size, int (*cmp)(int, int))
 	}
 }
 
+void Solution::ShellInsert(int * arr, int size, int (*cmp)(int, int), int d)
+{
+	for (int i = 0; i < size - d; ++i) {
+		if (cmp(arr[i], arr[i+d]) > 0) {
+			int j = i; 
+			int cache = arr[j+d];			
+
+			do {
+				arr[j+d] = arr[j];
+				j -= d;
+			} while (j >= 0 && cmp(arr[j], cache) > 0);
+
+			arr[j+d] = cache; 
+		}
+	}	
+}
+
+void Solution::ShellSort(int *arr, int size, int (*cmp)(int, int ))
+{
+	int d_list[] = {5, 3, 1};
+	int len = sizeof(d_list) / sizeof(int);
+	for (int i = 0; i < len; ++i) {
+		ShellInsert(arr, size, cmp, d_list[i]);
+	}
+}
+
+
+
+void Solution::CounterRadixSort(int *from, int * to, int size, int (*cmp)(int, int), int *count, int *pos, int r, int m)
+{
+	// 清零
+	bzero(count, sizeof(int) * r);
+	bzero(pos, sizeof(int) * r);
+
+	// 计算除数
+	int divisor = pow(10, m);		
+
+	// 收集过程， 计算个数	
+	for (int i = 0; i < size; ++i) {
+		++count[(from[i] / divisor) % 10];
+	}	
+
+	// 计算位置
+	for (int i = 1; i < r; ++i) {
+		pos[i] = pos[i - 1] + count[i - 1];
+	}
+
+	// 分配过程
+	for (int i = 0; i <size; ++i) {
+		to[pos[(from[i] / divisor) % 10]++] = from[i];
+	}	
+}
+
+void Solution::RadixSort(int *arr, int size, int (*cmp)(int, int ))
+{
+	if (size <= 1) {
+		return ;
+	}
+
+	// 基数排序需要事先知道基数有多少, 这里10进制整数， 基数0-9
+	int r = 10;
+
+	// 这里预先假定最大整数位9999，最大位数为4
+	int m = 4;
+
+	int *tmp_arr = (int*)malloc(sizeof(int) * size);
+	int *count = (int*)malloc(sizeof(int) * r);	
+	int *pos = (int*)malloc(sizeof(int) * r);	
+	if (!tmp_arr || !count || !pos) {
+		cerr << "malloc failed" << endl;
+		exit(EXIT_FAILURE);	
+	}
+
+	int i = 0;
+	while (i < m) {
+		if (i%2) {
+			CounterRadixSort(tmp_arr, arr, size, cmp, count, pos, r, i);
+		} else {
+			CounterRadixSort(arr, tmp_arr, size, cmp, count, pos, r, i);
+		}	
+
+		++i;
+	}
+
+	if (i%2) {
+		// copy 回来
+		memcpy(arr, tmp_arr, size * sizeof(int));
+	}
+}
+
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		cout << "usage : " << argv[0] << " count" << endl;
@@ -129,7 +230,7 @@ int main(int argc, char *argv[]) {
 	}	
 
 	Solution obj;
-	obj.InsertSort(arr, count, Compare);
+	obj.RadixSort(arr, count, Compare);
 	PrintArray(arr, count);	
 	return 0;
 }
