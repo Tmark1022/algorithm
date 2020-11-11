84d9ab5f68e88e4411909140947a15a05e4f298f
tmark's company <779297395@qq.com>
2020-11-11 18:18:23 +0800

    C++ 一些测试代码

---
+++ test/container.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+ @ Author	: tmark
+ @ Created Time	: Wed 11 Nov 2020 05:37:03 PM CST
+ @ File Name	: container.cpp
+ @ Description	: 
+ ************************************************************************/
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
---
+++ test/cpp_constructor.cpp
@@ -0,0 +1,52 @@
+/*************************************************************************
+ @ Author	: tmark
+ @ Created Time	: Wed 11 Nov 2020 05:21:57 PM CST
+ @ File Name	: cpp_constructor.cpp
+ @ Description	: 
+ ************************************************************************/
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+class Type {
+public:
+	Type() {	
+		cout << "default constructor " << endl;
+	}
+		
+	// 转换构造函数 
+	Type(int a, double b = 111.111):a(a), b(b) {
+		cout << "converting/direct constructor" << endl;
+	}; 
+	Type(const Type &obj):a(obj.a), b(obj.b) {
+		cout << "copy constructor" << endl;
+	}
+
+
+	void Print() const  {
+		cout << "a = " << a << "b = " << b << endl;
+	}
+	
+private:
+	int a;
+	double b;
+};
+
+// 测试转换构造函数
+// 这里形参不需要加const， 不加const 的引用类型需要被绑定值一定要同样类型； 加了const只要求表达式能够转换为对应类型即可， 所以这里就会使用转换构造函数转换为需要的类型， 完成函数调用。
+void foo(const Type &obj)
+{
+	obj.Print();
+}
+
+
+int main(int argc, char *argv[]) {
+	foo(123);	
+	Type obj = {1};
+	obj.Print();
+		
+	return 0;
+}
