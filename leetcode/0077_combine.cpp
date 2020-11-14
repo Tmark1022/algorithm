9208dcada442be399e228a245017187276a64b3d
tmark's company <779297395@qq.com>
2020-11-14 22:22:57 +0800

    提交leetcode 77

---
+++ leetcode/0077_combine.cpp
@@ -0,0 +1,59 @@
+/*************************************************************************
+ @ Author	: tmark
+ @ Created Time	: Sat 14 Nov 2020 07:28:18 PM CST
+ @ File Name	: 0077_combine.cpp
+ @ Description	: 77. 组合 
+ ************************************************************************/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+
+// solution 1: 递归模拟多层循环, 每一层递归就一层循环	
+class Solution {
+public:
+	void Help(int level, int start, int n, int k, vector<int> &tmp, vector<vector<int>> &ans) {
+		if (level == k) {
+			ans.push_back(tmp);
+			return ;
+		}
+		for (int i = start; i <= n - k + 1 + level; ++i) {
+			tmp[level] = i; 
+			Help(level + 1, i+1, n, k, tmp, ans);
+		}
+	}	
+
+    vector<vector<int>> combine(int n, int k) {
+	// invalid
+	if (k <= 0 || n < k) {
+		return {};
+	}	
+	vector<int> tmp(k); 
+	vector<vector<int>> ans;		
+	Help(0, 1, n, k, tmp, ans);
+	return ans;
+    }
+};
+
+int main(int argc, char *argv[]) {
+	int n = 4, k = 2;
+	Solution obj;
+
+	vector<vector<int>> ans = obj.combine(n, k);
+	for (auto &a : ans) {
+		cout << "[";
+		for (auto &b :a) {
+			cout << b << ",";
+		}
+		cout << "]" << endl;
+	}
+
+	return 0;
+}
