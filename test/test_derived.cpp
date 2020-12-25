fa3278b553c9bf2b48e810cef53d5086d3591bab
tmark's company <779297395@qq.com>
2020-12-25 20:17:29 +0800

    test code

---
+++ test/test_derived.cpp
@@ -0,0 +1,88 @@
+/*************************************************************************
+ @ Author	: tmark
+ @ Created Time	: Fri 25 Dec 2020 05:47:23 PM CST
+ @ File Name	: test_derived.cpp
+ @ Description	: 
+ ************************************************************************/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+
+class Base{
+public:
+	Base() = default;
+	
+	void vfunc() {
+		cout << "base vfunc" << endl;
+	}
+
+	static int sta;
+protected:
+	int share;
+private:
+	int a;
+	int b;
+};
+
+class Derived: public Base{
+public:
+	Derived():Base(){}
+
+	virtual void vfunc(){
+		cout << "derived vfunc" << endl;
+	}
+
+protected:
+	void foo() {
+		cout << this->share << endl;
+	}
+
+private:
+	int c;
+};
+
+class Derived2: public Derived{
+public:
+	Derived2():Derived(){}
+
+	void vfunc(){
+		cout << "derived222 vfunc" << endl;
+	}
+
+protected:
+	void foo() {
+		cout << this->share << endl;
+	}
+
+private:
+	int c;
+};
+
+int Base::sta = 100;
+int main(int argc, char *argv[]) {
+	Base ba;	
+	Derived der;
+	Derived2 der2;
+	ba.vfunc();	
+	der.vfunc();
+	der2.vfunc();
+
+	Derived &r = der2;
+	r.vfunc();
+
+	cout << der.sta << endl;
+	cout << der2.sta << endl;
+	der.sta = 89;
+	cout << der.sta << endl;
+	cout << der2.sta << endl;
+
+	return 0;
+}
