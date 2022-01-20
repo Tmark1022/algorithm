2c81b71af3811825a4f1b41ad61aeb397842a51d
tmark's company <779297395@qq.com>
2022-01-20 19:47:35 +0800

    allocator测试代码

--- test/test_allocator.cpp
+++ test/test_allocator.cpp
@@ -16,18 +16,38 @@
 
 using namespace std;
 
+class MyString : public string {
+	public:
+	using string::string;		// 继承的构造函数
 
-
-
+	~MyString() {
+		cout << "destory MyString : " << this << endl;
+	}	
+};
+	
 int main(int argc, char *argv[]) {
 
-	allocator<string> alloc;
-	string *p = alloc.allocate(10);
+	allocator<MyString> alloc;
+	MyString *p = alloc.allocate(1);
 	alloc.construct(p, 10, 'c');
 	cout << *p << endl;
+
+
+	MyString * p10 = alloc.allocate(10);
+	uninitialized_fill_n(p10, 10, *p);	
+
+	cout << "-------------------------------------------------" << endl;
+	p10[2] += "hello world"; 
+	for (MyString* beginptr = p10, *endptr = p10 + 10; beginptr != endptr; ++beginptr) {
+		cout << *beginptr << endl;
+
+		// destroy
+		alloc.destroy(beginptr);
+	}
+		
 	alloc.destroy(p);
-	alloc.deallocate(p, 10);
-	
+	alloc.deallocate(p, 1);	
+	alloc.deallocate(p10, 10);
 
 	return 0;
 }
