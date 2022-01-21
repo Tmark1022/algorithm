ce36aa2388fe7d8f9525bfa10c3fe89f6ce8bb8d
tmark's company <779297395@qq.com>
2022-01-21 17:44:12 +0800

    template specialization

--- test/template_specialization.cpp
+++ test/template_specialization.cpp
@@ -41,6 +41,44 @@ class Tmark<T *> {
 		}
 };
 
+/***************************************************
+* 2222222222222222222222 
+***************************************************/
+template<class T, class Q> 
+class Base {
+	public:
+		static void print() {
+			cout << "1111" << endl;
+		}
+};
+
+// Q 是double 的特例化
+template<class T>
+class Base<T, double> {
+	public:
+		static void print() {
+			cout << "2222" << endl;
+		}
+};
+
+// T和Q都是指针的特例化
+template<class T, class Q>
+class Base<T*, Q*> {
+	public:
+		static void print() {
+			cout << "3333" << endl;
+		}
+};
+
+// T是int， Q 是float的特例化
+template<>
+class Base<int, float> {
+	public:
+		static void print() {
+			cout << "444444444" << endl;
+		}
+};
+
 
 int main(int argc, char *argv[]) {
 	Tmark<char>().func();
@@ -51,6 +89,17 @@ int main(int argc, char *argv[]) {
 	Tmark<float>().func();
 	Tmark<double>().func();
 	Tmark<double *>().func();
+
+	cout << "------------------------ split line -----------------------" << endl;
+	Base<int, int>::print();
+	Base<int, float>::print();
+	Base<float, int>::print();
+	Base<int, Tmark<double>>::print();
+	Base<int *, int *>::print();
+	Base<int, double *>::print();
+	Base<int, double>::print();
+	Base<double, int>::print();
+
 
 	return 0;
 }
