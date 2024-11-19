e6bcc7ee951d27076f6448bb1c919599fd74f2ca
tmark's company <779297395@qq.com>
2024-11-19 21:56:29 +0800

    0239 添加新做法

--- leetcode/0239_maxSlidingWindow.cpp
+++ leetcode/0239_maxSlidingWindow.cpp
@@ -62,7 +62,24 @@ public:
     }
     */
 
-	// solution 2: 单调队列; 相对于暴力， 要提高效率可以从如何更快地从滑动窗口中找到最大值;
+	/*
+    // solution 2: 使用priority_queue 替代solution1中的内层循环， log(n) 时间复杂度入队， 然后O(1)找出最大值 
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        auto cmp = [](const pair<int, int> &a, const pair<int, int> &b){return a.first <= b.first;};
+        priority_queue<pair<int, int>, vector<pair<int,int>>, decltype(cmp)> pq(cmp);
+
+        for (int i = 0; i < k - 1; ++i) pq.push({nums[i], i});
+        for (int i = k-1; i < nums.size(); ++i) {
+            pq.push({nums[i], i});
+            while (pq.top().second <= i - k) pq.pop();
+            res.push_back(pq.top().first);
+        } 
+        return res; 
+    }
+    */
+
+	// solution 3: 单调队列; 相对于暴力， 要提高效率可以从如何更快地从滑动窗口中找到最大值;
 	// 这里我们可以借助递减队列(单调队列)来快速找到最大值， 递减队列存储的是滑动窗口中可能作为下一个最大值的值下标， 队头是当前滑动窗口的最大值 
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
 	vector<int> res;
@@ -80,7 +97,7 @@ public:
     }
 
 	/*
-	// solution 3: dp, 将数组划分为大小k的子数组， 
+	// solution 4: dp, 将数组划分为大小k的子数组， 
 	// 分别计算left数组(所在的K大小子数组左侧到当前下标的最大值) 和 right数组(所在的K大小子数组右侧到当前下标的最大值)
 	//
 	// 那么对于大小为K的[li, ri]滑动窗口中， 最大值就是max(right[li], left[ri]) 
